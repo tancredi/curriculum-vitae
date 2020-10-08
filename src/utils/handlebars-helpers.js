c9cc3e7392f52f9901930bbe744c2ddd9abdc07d
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2020-10-08 02:37:48 +0200

    Add jobs and logos

--- src/utils/handlebars-helpers.js
+++ src/utils/handlebars-helpers.js
@@ -1,1 +1,16 @@
+const slug = require('slug');
+const moment = require('moment');
+
 exports.lower = (str) => str.toLowerCase();
+
+exports.fallback = (str, fallback) => str || fallback;
+
+exports.date = (str, format, fallback) => {
+  if (!str) {
+    return fallback;
+  }
+
+  return moment(str, 'YYYY-MM-DD').format(format);
+};
+
+exports.slug = (str) => slug(str);
